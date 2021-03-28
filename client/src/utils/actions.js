242d14aee64d5e5d0657488f124d906a59989cc7
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-03-28 10:48:03 -0400

    added global state object and refactor

---
+++ client/src/utils/actions.js
@@ -0,0 +1,4 @@
+//with these three actions we define how 3 parts of our state will be maintained and updated
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
